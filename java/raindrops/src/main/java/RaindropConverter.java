b7f0d4a35eb25b5a89ca76c5b36594f5d26dee97
Graham Dennis <gdennis@palantir.com>
2018-12-23 11:59:25 +1000

    Java: raindrop

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,26 @@
 class RaindropConverter {
 
+    boolean isDivisibleBy(int number, int divisor) {
+        return (number % divisor) == 0;
+    }
+
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuilder stringBuilder = new StringBuilder();
+        if (isDivisibleBy(number, 3)) {
+            stringBuilder.append("Pling");
+        }
+        if (isDivisibleBy(number, 5)) {
+            stringBuilder.append("Plang");
+        }
+        if (isDivisibleBy(number, 7)) {
+            stringBuilder.append("Plong");
+        }
+        String result = stringBuilder.toString();
+        if (!result.isEmpty()) {
+            return result;
+        } else {
+            return Integer.toString(number);
+        }
     }
 
 }
