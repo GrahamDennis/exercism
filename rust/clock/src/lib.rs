733efe26c2b27fdd3ca7ec8838a03817cc645549
Graham Dennis <gdennis@palantir.com>
2018-12-24 22:27:30 +1000

    rust: clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,40 @@
-pub struct Clock;
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
+
+fn div_euc(dividend: i32, divisor: i32) -> i32 {
+    let q = dividend / divisor;
+    if dividend % divisor < 0 {
+        return q - 1
+    }
+    q
+}
+
+fn mod_euc(dividend: i32, divisor: i32) -> i32 {
+    let result = dividend % divisor;
+    if result < 0 {
+        return result + divisor;
+    }
+    result
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock { 
+            hours: mod_euc(hours + div_euc(minutes, 60), 24), 
+            minutes: mod_euc(minutes, 60) }
     }
 
     pub fn add_minutes(self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+
+}
+
+impl std::fmt::Display for Clock {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
