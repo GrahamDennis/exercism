d8b38571192286decbc79da37a0e56865787dd4e
Graham Dennis <graham.dennis@gmail.com>
2018-12-30 11:19:49 +1100

    js: improved leap?

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,10 +1,3 @@
-export const isLeap = (year) => {
-    // if year is divisible by 400, then it's a leap year
-    if ((year % 400) === 0) return true;
-    // otherwise if it's divisible by 100, then it isn't
-    if ((year % 100) === 0) return false;
-    // otherwise if it's divisible by 4, then it is
-    if ((year % 4) === 0) return true;
-    // otherwise it's not a leap year
-    return false;
-}
+const isDivisibleBy = (divisor) => (dividend) => (dividend % divisor) === 0;
+
+export const isLeap = (year) => isDivisibleBy(400)(year) || isDivisibleBy(4)(year) && !isDivisibleBy(100)(year);
