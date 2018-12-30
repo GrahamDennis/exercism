cb9b5d274379313aa735416799b15756944675ed
Graham Dennis <graham.dennis@gmail.com>
2018-12-31 10:40:08 +1100

    js: minor tweak to leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,4 +1,4 @@
 export const isLeap = (year) => {
     const isDivisibleBy = (divisor) => year % divisor === 0;
-    return isDivisibleBy(400) || isDivisibleBy(4) && !isDivisibleBy(100);
+    return isDivisibleBy(4) && (!isDivisibleBy(100) || isDivisibleBy(400));
 };
