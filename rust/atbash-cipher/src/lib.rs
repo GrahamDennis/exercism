22bc68e1e29c6eb591ff12548e791d0548664ceb
Graham Dennis <graham.dennis@gmail.com>
2018-12-31 11:28:04 +1100

    rust: atbash: replace hashmap with filter_map and a match statement

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,9 +1,7 @@
-use lazy_static::lazy_static;
-use std::collections::HashMap;
-
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    transpose(plain)
+    plain.chars()
+        .filter_map(transpose)
         .enumerate()
         .flat_map(|(idx, c)| if idx > 0 && idx % 5 == 0 { vec![' ', c] } else { vec![c] })
         .collect()
@@ -11,26 +9,16 @@ pub fn encode(plain: &str) -> String {
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    transpose(cipher).collect()
+    cipher.chars()
+        .filter_map(transpose)
+        .collect()
 }
 
-static ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
-
-lazy_static! {
-    static ref REVERSED_ALPHABET: String = ALPHABET.chars().rev().collect();
-
-    static ref CIPHER: HashMap<char, char> = {
-        let mut m = HashMap::new();
-        for (from, to) in ALPHABET.chars().zip(REVERSED_ALPHABET.chars()) {
-            m.insert(from, to);
-        }
-        m
-    };
-}
-
-fn transpose<'a>(input: &'a str) -> impl Iterator<Item=char> + 'a {
-    input.chars()
-        .filter(|c| c.is_ascii_alphanumeric())
-        .map(|c| c.to_ascii_lowercase())
-        .map(|c| *CIPHER.get(&c).unwrap_or(&c))
+fn transpose(c: char) -> Option<char> {
+    match c {
+        '0'...'9' => Some(c),
+        'a'...'z' => Some((b'a' + b'z' - c as u8) as char),
+        'A'...'Z' => Some((b'a' + b'Z' - c as u8) as char),
+        _ => None
+    }
 }
