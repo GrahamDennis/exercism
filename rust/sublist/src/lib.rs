d22048a664633cc5f5b11b6de202f251a7270179
Graham Dennis <graham.dennis@gmail.com>
2019-01-01 11:29:22 +1100

    rust: sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -10,20 +10,29 @@ pub enum Comparison {
 
 pub fn sublist<T: PartialEq>(first: &[T], second: &[T]) -> Comparison {
     match first.len().cmp(&second.len()) {
-        Ordering::Less => if is_strict_sublist(first, second) { return Comparison::Sublist },
-        Ordering::Equal => if first.eq(second) { return Comparison::Equal },
-        Ordering::Greater => if is_strict_sublist(second, first) { return Comparison::Superlist },
+        Ordering::Less => {
+            if is_strict_sublist(first, second) {
+                return Comparison::Sublist;
+            }
+        }
+        Ordering::Greater => {
+            if is_strict_sublist(second, first) {
+                return Comparison::Superlist;
+            }
+        }
+        Ordering::Equal => {
+            if first == second {
+                return Comparison::Equal;
+            }
+        }
     }
 
     Comparison::Unequal
 }
 
 fn is_strict_sublist<T: PartialEq>(needle: &[T], haystack: &[T]) -> bool {
-    for idx in 0..=(haystack.len() - needle.len()) {
-        if haystack[idx..].starts_with(needle) {
-            return true
-        }
-    }
-
-    false
+    needle.is_empty()
+        || haystack
+            .windows(needle.len())
+            .any(|candidate| candidate == needle)
 }
